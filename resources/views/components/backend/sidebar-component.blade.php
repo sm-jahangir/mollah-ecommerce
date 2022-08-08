bc83a57ca0d41703208fc8a32706a2a17d3c9478
sm-jahangir <samraatjahangir@gmail.com>
2022-08-08 14:43:49 +0600

    active class

--- resources/views/components/backend/sidebar-component.blade.php
+++ resources/views/components/backend/sidebar-component.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<!-- Brand Logo -->
-	<a href="index3.html" class="brand-link">
+	<a href="{{ route('app.dashboard') }}" class="brand-link">
 		<img src="{{ asset('assets/backend') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-		<span class="brand-text font-weight-light">AdminLTE 3</span>
+		<span class="brand-text font-weight-light">Molla Ecom</span>
 	</a>
 
 	<!-- Sidebar -->
@@ -13,56 +13,27 @@
 				<img src="{{ asset('assets/backend') }}/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
 			</div>
 			<div class="info">
-				<a href="#" class="d-block">Alexander Pierce</a>
-			</div>
-		</div>
-
-		<!-- SidebarSearch Form -->
-		<div class="form-inline">
-			<div class="input-group" data-widget="sidebar-search">
-				<input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-				<div class="input-group-append">
-					<button class="btn btn-sidebar">
-						<i class="fas fa-search fa-fw"></i>
-					</button>
-				</div>
+				<a href="#" class="d-block">Jahangir Alam</a>
+				<small class="text-white">{{ Auth::user()->role->name }}</small>
 			</div>
 		</div>
 
 		<!-- Sidebar Menu -->
 		<nav class="mt-2">
 			<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-				<!-- Add icons to the links using the .nav-icon class
-																																																				with font-awesome or any other icon font library -->
-				<li class="nav-item menu-open">
-					<a href="#" class="nav-link active">
+				<li class="nav-item">
+					<a href="{{ route('app.dashboard') }}" class="nav-link {{ Request::is('app/dashboard') ? 'active' : '' }}">
 						<i class="nav-icon fas fa-tachometer-alt"></i>
 						<p>
-							Starter Pages
-							<i class="right fas fa-angle-left"></i>
+							Dashboard
 						</p>
 					</a>
-					<ul class="nav nav-treeview">
-						<li class="nav-item">
-							<a href="#" class="nav-link active">
-								<i class="far fa-circle nav-icon"></i>
-								<p>Active Page</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="#" class="nav-link">
-								<i class="far fa-circle nav-icon"></i>
-								<p>Inactive Page</p>
-							</a>
-						</li>
-					</ul>
 				</li>
-				<li class="nav-item">
-					<a href="#" class="nav-link">
-						<i class="nav-icon fas fa-th"></i>
+				<li class="nav-item  {{ Request::is('app/roles*') ? 'menu-open' : '' }}">
+					<a href="{{ route('app.roles.index') }}" class="nav-link  {{ Request::is('app/roles*') ? 'active' : '' }}">
+						<i class="nav-icon fas fa-tachometer-alt"></i>
 						<p>
-							Simple Link
-							<span class="right badge badge-danger">New</span>
+							Roles
 						</p>
 					</a>
 				</li>
